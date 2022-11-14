0dc25c57e1bd1599e1d4c95389cdd49996aa89c4
L-N1988 <2994567011@qq.com>
2022-11-15 00:24:17 +0800

    my v2 emacs config.

---
+++ lisp/init-locale.el
@@ -0,0 +1,8 @@
+;; ... your code here ...
+(provide 'init-local)
+
+;;; Agenda settings
+(custom-set-variables
+ '(org-agenda-files (list
+                     "~/org/my-life.org"
+                     "~/org/triage.org")))
