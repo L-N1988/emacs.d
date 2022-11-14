ba602a5161b148fe5baf4e0576bcec6c2b844030
L-N1988 <2994567011@qq.com>
2022-11-15 00:14:53 +0800

    remove v1 emacs config.

--- lisp/init-locale.el
+++
@@ -1,8 +0,0 @@
-;; ... your code here ...
-(provide 'init-local)
-
-;;; Agenda settings
-(custom-set-variables
- '(org-agenda-files (list
-                     "~/org/my-life.org"
-                     "~/org/triage.org")))
