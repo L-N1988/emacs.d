cf1b5aed81a191eab97c503512cf8850299ebead
L-N1988 <2994567011@qq.com>
2022-11-15 12:52:13 +0800

    modify org-capture template.

--- lisp/init-locale.el
+++ lisp/init-locale.el
@@ -1,2 +1,15 @@
-;; ... your code here ...
+;; Activate org-zotxt-mode in org-mode buffers
+(add-hook 'org-mode-hook (lambda () (org-zotxt-mode 1)))
+;; Change citation format to be less cumbersome in files.
+;; You'll need to install mkbehr-short into your style manager first.
+(eval-after-load "zotxt"
+  '(setq zotxt-default-bibliography-style "mkbehr-short"))
+
+;; out magic for latex files
+(add-hook 'latex-mode-hook
+          (lambda ()
+            (outline-minor-mode 1)
+            (setq-local outline-regexp "\\\\\\(sub\\)*section\\>")
+            (setq-local outline-level (lambda () (- (outline-level) 7)))))
+
 (provide 'init-local)
